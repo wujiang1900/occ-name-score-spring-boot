18c21a21ac7552668232a96da686318af24baa0f
Jiang Wu <864216@cognizant.com>
2020-05-10 09:37:06 -0500

    initial cucumber setup

---
+++ src/test/java/com/occ/namescore/rest/feature/NamescoreRestApiTest.java
@@ -0,0 +1,12 @@
+package com.occ.namescore.rest.feature;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:Feature")
+public class NamescoreRestApiTest {
+
+}
---
+++ src/test/java/resource/feature/namescore.feature
@@ -0,0 +1,6 @@
+Feature:  upload endpoint uploads an input file and calculates the scores
+
+	Scenario: if file not found, throw exception
+		Given
+		When
+		Then
